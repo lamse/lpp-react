df1ee1456ab2ff1a1076cf3da2716507e87ff97e
kap <lamse@naver.com>
2025-08-12 10:05:25 +0900

    로그인 상태를 세션에 저장하도록 변경

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import {createJSONStorage, persist} from 'zustand/middleware';
 
 interface AuthState {
   isLoggedIn: boolean;
@@ -16,6 +16,7 @@ const useAuthStore = create<AuthState>()(
     }),
     {
       name: 'auth-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
 );
